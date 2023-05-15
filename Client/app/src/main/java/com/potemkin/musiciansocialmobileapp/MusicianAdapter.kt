d05b331842af0681e2d1cb1fed6a0ac88a11dc1e
pansanek <alex.potjomkin@gmail.com>
2023-05-15 20:05:57 +0300

    Seperated Server and Client

---
+++ Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/MusicianAdapter.kt
@@ -0,0 +1,67 @@
+package com.potemkin.musiciansocialmobileapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.musician_card.view.*
+import java.util.*
+
+class MusicianAdapter (
+    val context: Context,
+    val item: ArrayList<MusicianModel>,
+    val listener: OnItemClickListener
+) :
+    RecyclerView.Adapter<MusicianAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.musician_card,
+            parent, false
+        )
+
+        return ViewHolder(itemView)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val item = item.get(position)
+        holder.name.text = item.name
+        holder.genres.text = item.genres
+        holder.instruments.text = item.instruments
+        Picasso.get().load(item.icon_url).into(holder.imV);
+    }
+
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        val name = itemView.name
+        val genres = itemView.genres
+        val instruments = itemView.instruments
+        val imV = itemView.imV
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != -1) {
+                listener.onItemClick(item,position)
+            }
+        }
+
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(item: ArrayList<MusicianModel>, position: Int)
+    }
+}
---
+++ Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/MusicianModel.kt
@@ -0,0 +1,9 @@
+package com.potemkin.musiciansocialmobileapp
+
+class MusicianModel (
+        val id: Int,
+        val icon_url: String,
+        val name:String,
+        val genres:String,
+        val instruments:String
+)
