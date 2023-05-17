43251e3a913158cfc3bc7561c17908e4fd097582
pansanek <alex.potjomkin@gmail.com>
2023-05-17 17:47:35 +0300

    Log/Reg UI

--- Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/LoginActivity.kt
+++ Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.potemkin.musiciansocialmobileapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.potemkin.musiciansocialmobileapp.registration.RegistrationActivity
+import java.util.ArrayList
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val url = "http://localhost:8081/musician/all-musicians"
+        val itemList: ArrayList<MusicianModel> = ArrayList()
     }
+
+    fun onClickReg(view: View) {
+        val i = Intent(this, RegistrationActivity::class.java)
+        startActivity(i)
+    }
+
 }
--- Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/registration/RegistrationActivity.kt
+++ Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/registration/RegistrationActivity.kt
@@ -1,12 +1,40 @@
 package com.potemkin.musiciansocialmobileapp.registration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import com.potemkin.musiciansocialmobileapp.LoginActivity
 import com.potemkin.musiciansocialmobileapp.R
+import com.potemkin.musiciansocialmobileapp.UserPageActivity
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        val items = listOf("Музыкант","Администратор базы","Администратор площадки")
+
+        val autoComplete : AutoCompleteTextView = findViewById(R.id.autoComplete)
+
+        val adapter = ArrayAdapter(this,R.layout.list_item,items)
+
+        autoComplete.setAdapter(adapter)
+
+        autoComplete.onItemClickListener = AdapterView.OnItemClickListener{
+            adapterView, view, i, l ->
+
+            val itemSelected = adapterView.getItemAtPosition(i)
+            Toast.makeText(this,"Item: " +itemSelected,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onClickLogin(view: View) {
+        val i = Intent(this, LoginActivity::class.java)
+        startActivity(i)
     }
 }
