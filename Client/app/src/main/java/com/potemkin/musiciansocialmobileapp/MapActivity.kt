bd5198eb50aafea07bd15c63f6691cfa6645cbd0
pansanek <alex.potjomkin@gmail.com>
2023-05-18 14:59:31 +0300

    Changed icons and added map functional

--- Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/MapActivity.kt
+++ Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/MapActivity.kt
@@ -1,26 +1,96 @@
 package com.potemkin.musiciansocialmobileapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapObjectTapListener
+import com.yandex.mapkit.map.PlacemarkMapObject
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MapActivity : AppCompatActivity() {
     lateinit var mapview:MapView
+    val yotcLat = 55.788089
+    val yotcLon = 37.705623
+    private var markerTapListener: MapObjectTapListener? = null
+    private var placeList = ArrayList<PlacemarkMapObject>()
     override fun onCreate(savedInstanceState: Bundle?) {
         MapKitFactory.setApiKey("cb7bdd48-6aa7-4812-ba6e-72902c458a81")
         MapKitFactory.initialize(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        placeInfo.visibility = View.GONE
         mapview = findViewById(R.id.mapView)
         mapview.map.move(CameraPosition(Point(55.754598, 37.619703),11.0f,0.0f,0.0f),
-        Animation(Animation.Type.SMOOTH,1f),null)
+        Animation(Animation.Type.SMOOTH,0f),null)
+        val RepBasePoint = Point(yotcLat, yotcLon)
+        val rbImageProvider: ImageProvider = ImageProvider
+            .fromResource(
+                this,
+                R.drawable.ic_rep_base)
+        mapview.map.mapObjects.addPlacemark(RepBasePoint,rbImageProvider).addTapListener { mapObject, point ->
+            showPlaceInfo()
 
+
+            true
+        }
+        val ConVenPoint = Point(55.791567, 37.682718)
+        val cvImageProvider: ImageProvider = ImageProvider
+            .fromResource(
+                this,
+                R.drawable.ic_con_venue)
+        mapview.map.mapObjects.addPlacemark(ConVenPoint,cvImageProvider).addTapListener { mapObject, point ->
+            showPlaceInfo()
+
+
+            true
+        }
+
+
+    }
+
+    private fun showPlaceInfo() {
+        placeInfo.visibility = View.VISIBLE
+
+        closeButton.setOnClickListener { placeInfo.visibility = View.GONE }
+    }
+
+    private fun setTapListener(listener: MapObjectTapListener) {
+        markerTapListener = listener
+    }
+
+    /*
+        Это обёртка над addPlacemark
+        в которой к маркеру цепляется обработчик событий
+        и полезная нагрузка
+    */
+    private fun addMarker(
+        latitude: Double,
+        longitude: Double,
+        @DrawableRes imageRes: Int,
+        userData: Any? = null
+    ): PlacemarkMapObject
+    {
+        val marker = mapview.map.mapObjects
+            .addPlacemark(
+                Point(latitude, longitude),
+                ImageProvider
+                    .fromResource(
+                        this,
+                        imageRes)
+            )
+        marker.userData = userData
+        markerTapListener?.let{
+            marker
+                .addTapListener(it) }
+        return marker
     }
 
     override fun onStart() {
