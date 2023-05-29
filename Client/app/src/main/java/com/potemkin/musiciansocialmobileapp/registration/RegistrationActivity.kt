61e2d6af7de318e503c89e4e0f85bfa66105ff62
pansanek <alex.potjomkin@gmail.com>
2023-05-29 23:59:32 +0300

    Починил ликвибейз и добавил тесты на клиенте

--- Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/registration/RegistrationActivity.kt
+++ Client/app/src/main/java/com/potemkin/musiciansocialmobileapp/registration/RegistrationActivity.kt
@@ -9,12 +9,16 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.potemkin.musiciansocialmobileapp.LoginActivity
 import com.potemkin.musiciansocialmobileapp.MapActivity
 import com.potemkin.musiciansocialmobileapp.R
 import com.potemkin.musiciansocialmobileapp.models.UserModel
 import kotlinx.android.synthetic.main.activity_place_registration.*
 import kotlinx.android.synthetic.main.activity_registration.*
+import org.json.JSONArray
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -86,23 +90,35 @@ class RegistrationActivity : AppCompatActivity() {
     }
 
     private fun regUser(user: UserModel) {
-        val url = URL("http://10.0.2.2:8081/users/upload-users")
-        val connection = url.openConnection() as HttpURLConnection
-        connection.requestMethod = "POST"
-        connection.setRequestProperty("Content-Type", "application/json")
-        connection.doOutput = true
-        val data =
-            "{\n        \"login\": \""+user.login+"\",\n        \"password\": \""+user.password+"\",\n        \"name\": \""+user.name+"\",\n        \"email\": \""+user.email+"\",\n        \"about\": \""+user.about+"\",\n        \"usersType\": \""+user.type+"\"\n    \n}"
-        val requestBodyBytes: ByteArray = data.toByteArray(StandardCharsets.UTF_8)
-        connection.getOutputStream().write(requestBodyBytes);
-        val responseCode = connection.responseCode
-        if (responseCode === HttpURLConnection.HTTP_OK) {
-            val `in` = BufferedReader(InputStreamReader(connection.inputStream))
-            val response: String = `in`.readLine()
-            return
-        } else {
-            System.out.println("Error: $responseCode")
-        }
+        val Queue = Volley.newRequestQueue(this)
+        val Url = "http://10.0.2.2:8081/users/all-users"
+        val Req = StringRequest(Request.Method.GET, Url, { response ->
+            val Data = response.toString()
+            val JsonArray = JSONArray(Data)
+            val Obj = JsonArray.getJSONObject(JsonArray.length() - 1)
+            val JsonId = Obj.getInt("usersId")
+            var userId = JsonId
+            val url = URL("http://10.0.2.2:8081/users/upload-users")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            connection.setRequestProperty("Content-Type", "application/json")
+            connection.doOutput = true
+            val data =
+                "{	\n  	\"usersId\": "+(userId+1)+",\n    \"login\": \""+user.login+"\",\n        \"password\": \""+user.password+"\",\n        \"name\": \""+user.name+"\",\n        \"email\": \""+user.email+"\",\n        \"about\": \""+user.about+"\",\n        \"usersType\": \""+user.type+"\"\n    \n}"
+            val requestBodyBytes: ByteArray = data.toByteArray(StandardCharsets.UTF_8)
+            connection.getOutputStream().write(requestBodyBytes);
+            val responseCode = connection.responseCode
+            if (responseCode === HttpURLConnection.HTTP_OK) {
+                val `in` = BufferedReader(InputStreamReader(connection.inputStream))
+                val response: String = `in`.readLine()
+            } else {
+                System.out.println("Error: $responseCode")
+            }
+        }, {
+            Toast.makeText(this, "Ошибка", Toast.LENGTH_SHORT).show() }
+        )
+        Queue.add(Req)
+
 
     }
 
--- Client/app/src/test/java/com/potemkin/musiciansocialmobileapp/ExampleUnitTest.kt
+++ Client/app/src/test/java/com/potemkin/musiciansocialmobileapp/ExampleUnitTest.kt
@@ -1,8 +1,22 @@
 package com.potemkin.musiciansocialmobileapp
 
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import org.junit.Test
 
 import org.junit.Assert.*
+import com.potemkin.musiciansocialmobileapp.models.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +24,89 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    internal var user:UserModel = UserModel(0,"","","","","","")
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun userModelCreatedCorrectly() {
+        var user: UserModel =
+            UserModel(1, "pansanek@email.com", "1234", "Im alex", "pansanek", "Alex", "M")
+        assertEquals(1, user.id)
+        assertEquals("pansanek@email.com", user.email)
+        assertEquals("1234", user.password)
     }
+
+    @Test
+    fun bandModelCreatedCorrectly() {
+        var band = BandModel("Metallica", "Heavy metal")
+        assertEquals("Metallica", band.name)
+        assertEquals("Heavy metal", band.genre)
+    }
+
+    @Test
+    fun testConnection() {
+        val url = URL("http://localhost:8081/users/auth-user")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.setRequestProperty("Content-Type", "application/json")
+        connection.doOutput = true
+        connection.getOutputStream()
+        val responseCode = connection.responseCode
+        assertEquals(400, responseCode)
+    }
+
+    @Test
+    fun authUserTest() {
+        val url = URL("http://localhost:8081/users/auth-user")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.setRequestProperty("Content-Type", "application/json")
+        connection.doOutput = true
+        var Email = "pansanek@gmail.com"
+        var Password = "12341234"
+        val data =
+            "{\"usersId\": 1,\n    \"login\": \"\",\n    \"password\": \"" + Password + "\",\n    \"name\": \"\",\n    \"email\": \"" + Email + "\",\n    \"about\": \"\",\n    \"usersType\": \"\"}"
+        val requestBodyBytes: ByteArray = data.toByteArray(StandardCharsets.UTF_8)
+        connection.getOutputStream().write(requestBodyBytes);
+        val `in` = BufferedReader(InputStreamReader(connection.inputStream))
+        val response: String = `in`.readLine()
+        assertEquals("OK", response)
+    }
+
+    @Test
+    fun authUserBadPasswordTest() {
+        val url = URL("http://localhost:8081/users/auth-user")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.setRequestProperty("Content-Type", "application/json")
+        connection.doOutput = true
+        var Email = "pansanek@gmail.com"
+        var Password = "1234"
+        val data =
+            "{\"usersId\": 1,\n    \"login\": \"\",\n    \"password\": \"" + Password + "\",\n    \"name\": \"\",\n    \"email\": \"" + Email + "\",\n    \"about\": \"\",\n    \"usersType\": \"\"}"
+        val requestBodyBytes: ByteArray = data.toByteArray(StandardCharsets.UTF_8)
+        connection.getOutputStream().write(requestBodyBytes);
+        val `in` = BufferedReader(InputStreamReader(connection.inputStream))
+        val response: String = `in`.readLine()
+        assertEquals("Bad password", response)
+    }
+
+    @Test
+    fun authUserBadEmailTest() {
+        val url = URL("http://localhost:8081/users/auth-user")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.setRequestProperty("Content-Type", "application/json")
+        connection.doOutput = true
+        var Email = "pansank@gmail.com"
+        var Password = "12341234"
+        val data =
+            "{\"usersId\": 1,\n    \"login\": \"\",\n    \"password\": \"" + Password + "\",\n    \"name\": \"\",\n    \"email\": \"" + Email + "\",\n    \"about\": \"\",\n    \"usersType\": \"\"}"
+        val requestBodyBytes: ByteArray = data.toByteArray(StandardCharsets.UTF_8)
+        connection.getOutputStream().write(requestBodyBytes);
+        val `in` = BufferedReader(InputStreamReader(connection.inputStream))
+        val response: String = `in`.readLine()
+        assertEquals("No such user", response)
+    }
+
+
 }
